d40b8060584897526d20a9382a2978fbafa15f34
barccoment <barccoment62@gmail.com>
2018-04-17 08:05:09 +0100

    BBWorld

---
+++ comment.js
@@ -0,0 +1,12 @@
+function commet()
+{
+var n=document.getElementById("name");
+var c = document.getElementById("comment");
+var d =alert("Thanks for Submitting Your Comment!");
+var b =document.getElementById("text");
+}
+function Pop()
+            {
+        var pop =document.getElementById("pop_ads");
+        pop.classList.toggle("show");
+            }
